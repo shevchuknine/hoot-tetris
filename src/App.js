030b2fff8cf271c416a45eb49e8536f94a7e0592
Evgeniy Shevchuk <shevchuknine@gmail.com>
2019-10-01 22:09:04 +0300

    initial version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import {filterFromFigure, printMap, returnInitialMap} from "./helpers";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import merge from "lodash.merge";
+
+const initialMap = returnInitialMap(),
+    figure = [[0, 0, 2, 2, 0], [0, 0, 0, 2, 0]];
+const App = () => {
+    const [map, setMap] = useState(initialMap);
+    const [currentRow, setCurrentRow] = useState(0);
+
+    useEffect(_ => {
+        setTimeout(() => {
+            // получаем состояние карты на след. шаге
+            const nextMapState =
+                filterFromFigure(map.slice(0, currentRow))
+                    .concat(figure) // конкатить нужно этот регион с мерже с фигурой через mergeWith
+                    .concat(map.slice(currentRow + figure.length, map.length));
+
+            // проверяем пересечения
+            let hasConflicts = false;
+            nextMapState.forEach((row, rowIndex) => {
+                row.forEach((item, itemIndex) => {
+                    hasConflicts = hasConflicts || (item === 2 && map[rowIndex][itemIndex] === 1);
+                });
+            });
+
+            // обновляем карту
+            const isEndOfField = currentRow + figure.length === 10;
+            if (hasConflicts || isEndOfField) {
+                setCurrentRow(0);
+                setMap(filterFromFigure(nextMapState, 1));
+            } else {
+                setCurrentRow(currentRow + 1);
+                setMap(nextMapState);
+            }
+        }, 1000);
+    });
+
+    console.log(printMap(map));
+
+    return null;
+};
 
 export default App;
---
+++ src/helpers.js
@@ -0,0 +1,20 @@
+export const returnInitialMap = () => {
+    return new Array(10).fill(new Array(5).fill(0));
+};
+
+export const filterFromFigure = (map, value = 0) => {
+    return map.map(row => {
+        return row.map(item => item === 2 ? value : item);
+    })
+};
+
+export const printMap = (map) => {
+    console.clear();
+
+    return map.reduce((rowStr, row) => {
+        return row.reduce((res, item, index) => {
+            const lastItem = index === row.length - 1;
+            return `${res} ${item}${lastItem ? "\n" : ""}`
+        }, rowStr)
+    }, "");
+};
