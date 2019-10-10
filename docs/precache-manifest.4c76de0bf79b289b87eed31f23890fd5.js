7b35a88a9a90f07e6911e4d7f346531f13596247
Evgeniy Shevchuk <shevchuknine@gmail.com>
2019-10-10 21:40:11 +0300

    [master] Make initial build

---
+++ docs/precache-manifest.4c76de0bf79b289b87eed31f23890fd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee2c29fe86a4b3c1ff2e8fe057ac6bca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f56e82c335c2bb7bfb5a",
+    "url": "/static/css/main.3744d4f6.chunk.css"
+  },
+  {
+    "revision": "12656b5eb5d8d13c8bb2",
+    "url": "/static/js/2.7730b68f.chunk.js"
+  },
+  {
+    "revision": "f56e82c335c2bb7bfb5a",
+    "url": "/static/js/main.f4e11b2f.chunk.js"
+  },
+  {
+    "revision": "d5e5cd626a372a71f9a8",
+    "url": "/static/js/runtime-main.c3a75ab0.js"
+  }
+]);
